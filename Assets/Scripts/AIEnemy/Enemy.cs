eb083840d691b8aecbfc16a2888d361997af5301
Jasiek <jan4000@o2.pl>
2019-12-13 18:00:01 +0100

    First tests of AI

---
+++ Assets/Scripts/AIEnemy/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject m_player;
+    private NavMeshAgent m_enemyAgent;
+
+    void Start()
+    {
+        m_enemyAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        m_enemyAgent.destination = m_player.transform.position;
+    }
+}
---
+++ Assets/Scripts/AIEnemy/PlayerForTestingAI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerForTestingAI : MonoBehaviour
+{
+    private Rigidbody m_playerBody;
+    [SerializeField]
+    float m_playerSpeed= 2f;
+    void Start()
+    {
+        m_playerBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){
+            MoveUp();
+        }
+        if (Input.GetKey(KeyCode.A)){
+            MoveLeft();
+        }
+        if (Input.GetKey(KeyCode.S)){
+            MoveDown();
+        }
+        if (Input.GetKey(KeyCode.D)){
+            MoveRight();
+        }
+    }
+
+    void MoveDown(){
+        m_playerBody.AddForce(Vector3.right*m_playerSpeed);
+    }
+
+    void MoveUp(){
+        m_playerBody.AddForce(Vector3.left*m_playerSpeed);
+    }
+
+    void MoveRight(){
+        m_playerBody.AddForce(Vector3.forward*m_playerSpeed);
+    }
+
+    void MoveLeft(){
+        m_playerBody.AddForce(Vector3.back*m_playerSpeed);
+    }
+}
