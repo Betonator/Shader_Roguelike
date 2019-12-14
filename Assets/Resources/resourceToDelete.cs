cb0d76063b7ba8627d038327dde207e27b1f84af
Jasiek <jan4000@o2.pl>
2019-12-14 20:08:16 +0100

    Repairing things

---
+++ Assets/Resources/resourceToDelete.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resourceToDelete : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
